8c0cb5474128a5dc9d8caf62868cc3717678e9f6
ilianhaggmark <ilianh@kth.se>
2016-09-17 14:50:23 +0200

    P1T1 Taylor Expansion with Horner's Scheme
    Taylor expansion of sin and cos using Horner's scheme. Previous problem with changing result was resolved with switching from array to vector in the functions sinTaylorH and cosTaylorH.

--- p1t1.cpp
+++ p1t1.cpp
@@ -1,21 +1,10 @@
 #include <iostream>
 #include <cmath>
-
+#include <conio.h> // loads getch();
+#include <vector>  // vec.clear();
 using namespace std;
+double hornerScheme(vector<double> Poly, int degree, double x);
 
-double hornerScheme(double * polynomCoef, const int pDegree, double x);
-
-
-
-// Horner's Scheme - Evaluated a polynom for a value x. Takes vector of polynomial coefficients (polynomCoef), 
-// the degree of the polynomial (pDegree) and an evaluation value (x). 
-double hornerScheme(double * polynomCoef, const int pDegree, double x) {
-  double hSchemeRes = 0.0;
-  for (int i=pDegree; i >=0; i--){
-    hSchemeRes = polynomCoef[i]+x*hSchemeRes;
-  }
-  return hSchemeRes;
-}
 
 // recursive factorial function returns double
 double factorial(int n)
@@ -23,7 +12,7 @@ double factorial(int n)
   return (n == 1 || n == 0) ? 1 : factorial(n - 1) * n;
 }
 
-// taylor expansion for sin
+// taylor expansion for sin using pow
 double sinTaylor(int N, double x) {
 double sum = 0;
   for (int n = 0; n <= N; n++)
@@ -31,7 +20,15 @@ double sum = 0;
   return sum;
 }
 
-// taylor expansion for cos
+// taylor expansion for sin with Horner's scheme
+double sinTaylorH(const int N, double x) {
+  vector<double> a (2*N); //numb. of polyn. coeficients should be the deg. + 1
+  for (int n = 0; n < N; n++)
+    a[2*n+1] = ( (1-2*(n%2)) / factorial(2 * n + 1));
+  return hornerScheme(a,2*N-1, x);
+}
+
+// taylor expansion for cos using pow
 double cosTaylor(int N, double x) {
 double sum = 0;
   for (int n = 0; n <= N; n++)
@@ -39,34 +36,44 @@ double sum = 0;
   return sum;
 }
 
-double sinTaylorH(int N, double x) {
-double a[2*N]; // number of polyn. coeficients should be the degree + 1
+// taylor expansion for sin with Horner's scheme
+double cosTaylorH(const int N, double x) {
+  vector<double> b (2*N); //numb. of polyn. coeficients should be the deg. + 1
   for (int n = 0; n < N; n++)
-    a[2*n+1] = ( (1-2*(n%2)) / factorial(2 * n + 1));
-  return hornerScheme(a,2*N-1, x);
+    b[2*n] = ( (1-2*(n%2)) / factorial(2 * n ));
+  return hornerScheme(b,2*N-2, x);
 }
 
+// Simple implmentation of Horner's Scheme
+double hornerScheme(vector<double> PolynomCoef, const int pDegree, double x) {
+  double hSchemeRes = 0.0;
+  for (int i=pDegree; i >=0; i--){
+    hSchemeRes = PolynomCoef[i]+x*hSchemeRes;
+  }
+  return hSchemeRes;
 
+}
 
 int main() {
   double factorial(int n);
   int N = 100;
-  double x = 3.1416;
-  cout << sinTaylor(N, x) << endl;
-  cout << cosTaylor(N, x) << endl;
-  cout << abs(sin(x) - sinTaylor(N, x)) << endl;
-  cout << abs(cos(x) - cosTaylor(N, x)) << endl;
-  
-  cout << "1 sin (Horner) = "<< sinTaylorH(N, x) << endl;
-  cout << "2 sin (Hroner) = "<< sinTaylorH(N, x) << endl;
-  
-  double poly[3];
+  double x = 3.1415; //x = 3.1416;
+  cout << "1 sin(x) = "<< sinTaylorH(N, x) << endl;
+  cout << "2 sin(x) = "<< sinTaylorH(N, x) << endl;
+  cout << "1 cos(x) = "<< cosTaylorH(N, x) << endl;
+  cout << "2 cos(x) = "<< cosTaylorH(N, x) << endl;
+  cout << "error sin " << abs(sin(x) - sinTaylorH(N, x)) << endl;
+  cout << "error cos " << abs(cos(x) - cosTaylor(N, x)) << endl;
+
+
+ // test of Horner's Scheme
+  vector<double> testvec (3);
   double y = 2;
-  b[0] = 5;
-  b[1] = 2;
-  b[2] = 3;
-  cout << hornerScheme(poly, 2, y) << endl;
-  
-  
+  testvec[0] = 5;
+  testvec[1] = 2;
+  testvec[2] = 3;
+  cout << "5+2x+3x^2 for x=2     " << hornerScheme(testvec, 2, y) << endl;
+
+  getch();
   return 0;
 }
