c53bb6c0be8215535aa5411247f125f80e7e3de4
ilianhaggmark <ilianh@kth.se>
2016-09-25 11:46:43 +0200

    Taylor with Horner's Scheme - updated factorial function

--- p1t1.cpp
+++ p1t1.cpp
@@ -12,20 +12,21 @@ double factorial(int n)
   return (n == 1 || n == 0) ? 1 : factorial(n - 1) * n;
 }
 
+// factorial function using for-loop. Returns double.
+double factorialFor(unsigned int n)
+{
+  //return (n == 1 || n == 0) ? 1 : factorial(n - 1) * n;
+  unsigned long long fac = 1;
+  for(unsigned int j = 1; j<=n; ++j) {fac *=j;}
+  return fac;
+}
+
 // taylor expansion for sin using pow
 double sinTaylor(int N, double x) {
 double sum = 0;
   for (int n = 0; n <= N; n++)
     sum += (pow(-1.0, n) * (pow(x, 2.0 * n + 1) / factorial(2 * n + 1)));
   return sum;
-}
-
-// taylor expansion for sin with Horner's scheme
-double sinTaylorH(const int N, double x) {
-  vector<double> a (2*N); //numb. of polyn. coeficients should be the deg. + 1
-  for (int n = 0; n < N; n++)
-    a[2*n+1] = ( (1-2*(n%2)) / factorial(2 * n + 1));
-  return hornerScheme(a,2*N-1, x);
 }
 
 // taylor expansion for cos using pow
@@ -37,11 +38,19 @@ double sum = 0;
 }
 
 // taylor expansion for sin with Horner's scheme
-double cosTaylorH(const int N, double x) {
-  vector<double> b (2*N); //numb. of polyn. coeficients should be the deg. + 1
-  for (int n = 0; n < N; n++)
-    b[2*n] = ( (1-2*(n%2)) / factorial(2 * n ));
-  return hornerScheme(b,2*N-2, x);
+double sinTaylorH(const int nTerms, double x) {
+  vector<double> a (2*nTerms); //numb. of polyn. coeficients should be the deg. + 1
+  for (int n = 0; n < nTerms; n++)
+    a[2*n+1] = ( (1-2*(n%2)) / factorialFor(2 * n + 1));
+  return hornerScheme(a,2*nTerms-1, x);
+}
+
+// taylor expansion for sin with Horner's scheme
+double cosTaylorH(const int nTerms, double x) {
+  vector<double> b (2*nTerms); //numb. of polyn. coeficients should be the deg. + 1
+  for (int n = 0; n < nTerms; n++)
+    b[2*n] = ( (1-2*(n%2)) / factorialFor(2 * n ));
+  return hornerScheme(b,2*nTerms-2, x);
 }
 
 // Simple implmentation of Horner's Scheme
@@ -56,10 +65,12 @@ double hornerScheme(vector<double> PolynomCoef, const int pDegree, double x) {
 
 int main() {
   double factorial(int n);
-  int N = 100;
+  int N = 10;
   double x = 3.1415; //x = 3.1416;
+  cout << "  sin(x) = "<< sinTaylor(N, x) << endl;
   cout << "1 sin(x) = "<< sinTaylorH(N, x) << endl;
   cout << "2 sin(x) = "<< sinTaylorH(N, x) << endl;
+  cout << "  cos(x) = "<< cosTaylor(N, x) << endl;
   cout << "1 cos(x) = "<< cosTaylorH(N, x) << endl;
   cout << "2 cos(x) = "<< cosTaylorH(N, x) << endl;
   cout << "error sin " << abs(sin(x) - sinTaylorH(N, x)) << endl;
