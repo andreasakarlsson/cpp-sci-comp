26cedf21f9a514f3a6a543e145472ec00d6317e6
andreasakarlsson <andreas.a.karlsson@gmail.com>
2016-09-26 21:06:14 +0200

    Tidied up. FactorialFor() returns 0 for large N
    factorialFor(N) starts to returns 0 as N increases this does not happen
    to the recursive implementation of factorial. Is this a type error?

--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 CC=g++
-CFLAGS=-g -Wall
+CFLAGS=-g -Wall -std=c++11
 
 p1t1.o: p1t1.cpp
 	$(CC) $(CFLAGS) p1t1.cpp -o p1t1.o
--- p1t1.cpp
+++ p1t1.cpp
@@ -2,50 +2,31 @@
 #include <cmath>
 #include <vector>  // vec.clear();
 using namespace std;
-double hornerScheme(vector<double> Poly, int degree, double x);
 
-
-// recursive factorial function returns double
-double factorial(int n)
-{
+// recursive factorial function. Returns double.
+double factorial(int n) {
   return (n == 1 || n == 0) ? 1 : factorial(n - 1) * n;
 }
 
 // factorial function using for-loop. Returns double.
-double factorialFor(unsigned int n)
-{
-  //return (n == 1 || n == 0) ? 1 : factorial(n - 1) * n;
+double factorialFor(unsigned int n) {
   unsigned long long fac = 1;
   for(unsigned int j = 1; j<=n; ++j) {fac *=j;}
   return fac;
 }
 
-// taylor expansion for sin using pow
-double sinTaylor(int N, double x) {
-double sum = 0;
-  for (int n = 0; n <= N; n++)
-    sum += (pow(-1.0, n) * (pow(x, 2.0 * n + 1) / factorial(2 * n + 1)));
-  return sum;
-}
-
-// taylor expansion for cos using pow
-double cosTaylor(int N, double x) {
-double sum = 0;
-  for (int n = 0; n <= N; n++)
-    sum += (pow(-1.0, n) * (pow(x, 2.0 * n) / factorial(2 * n)));
-  return sum;
-}
-
 // taylor expansion for sin with Horner's scheme
 double sinTaylorH(const int nTerms, double x) {
-  vector<double> a (2*nTerms); //numb. of polyn. coeficients should be the deg. + 1
+  double hornerScheme(vector<double> Poly, int degree, double x);
+  vector<double> a (2*nTerms); //numb. of polyn. coefficients should be the deg. + 1
   for (int n = 0; n < nTerms; n++)
-    a[2*n+1] = ( (1-2*(n%2)) / factorialFor(2 * n + 1));
+    a[2*n+1] = ( (1-2*(n%2)) / factorial(2 * n + 1));
   return hornerScheme(a,2*nTerms-1, x);
 }
 
 // taylor expansion for sin with Horner's scheme
 double cosTaylorH(const int nTerms, double x) {
+  double hornerScheme(vector<double> Poly, int degree, double x);
   vector<double> b (2*nTerms); //numb. of polyn. coeficients should be the deg. + 1
   for (int n = 0; n < nTerms; n++)
     b[2*n] = ( (1-2*(n%2)) / factorialFor(2 * n ));
@@ -59,30 +40,22 @@ double hornerScheme(vector<double> PolynomCoef, const int pDegree, double x) {
     hSchemeRes = PolynomCoef[i]+x*hSchemeRes;
   }
   return hSchemeRes;
-
 }
 
 int main() {
-  double factorial(int n);
-  int N = 10;
-  double x = 3.1415; //x = 3.1416;
-  cout << "  sin(x) = "<< sinTaylor(N, x) << endl;
-  cout << "1 sin(x) = "<< sinTaylorH(N, x) << endl;
-  cout << "2 sin(x) = "<< sinTaylorH(N, x) << endl;
-  cout << "  cos(x) = "<< cosTaylor(N, x) << endl;
-  cout << "1 cos(x) = "<< cosTaylorH(N, x) << endl;
-  cout << "2 cos(x) = "<< cosTaylorH(N, x) << endl;
-  cout << "error sin " << abs(sin(x) - sinTaylorH(N, x)) << endl;
-  cout << "error cos " << abs(cos(x) - cosTaylor(N, x)) << endl;
+  vector<double> x_vec = {-1, 1, 2, 3, 5, 10};
+  vector<unsigned int> N_vec = {1, 20, 100, 1000};
 
-
- // test of Horner's Scheme
-  vector<double> testvec (3);
-  double y = 2;
-  testvec[0] = 5;
-  testvec[1] = 2;
-  testvec[2] = 3;
-  cout << "5+2x+3x^2 for x=2     " << hornerScheme(testvec, 2, y) << endl;
-
+  // Learning ranges in C++11
+  for (auto N: N_vec) {
+    cout << "Setting N = " << N << endl;
+    for (auto x: x_vec) {
+      cout << "  Setting x = " << x << endl;
+      cout << "    sin using Horner, recursive factorial, error:     "
+	   << abs(sin(x) - sinTaylorH(N, x)) << endl;
+      cout << "    cos using Horner, factorial with for-loop, error: "
+	   << abs(cos(x) - cosTaylorH(N, x)) << endl;
+    }
+  }
   return 0;
 }
