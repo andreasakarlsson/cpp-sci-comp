8828d8ccbec096b7369398304815ab4389441e30
andreasakarlsson <andreas.a.karlsson@gmail.com>
2016-09-29 17:58:10 +0200

    Simplification of project 1 task 1

--- p1t1.cpp
+++ p1t1.cpp
@@ -1,38 +1,34 @@
 #include <iostream>
 #include <cmath>
-#include <vector>  // vec.clear();
+#include <vector>
 using namespace std;
 
-
 // taylor expansion for sin with Horner's scheme
-double sinTaylorH(const int nTerms, double x) {
-  double hornerScheme(vector<double> Poly, int degree, double x);
-  vector<double> a (2*nTerms); //numb. of polyn. coefficients should be the deg. + 1
-  for (int n = 0; n < nTerms; n++)
-    a[ 2 * n + 1] = ( (1-2*(n%2)));
-  return hornerScheme(a,2*nTerms-1, x);
-}
-
-// taylor expansion for sin with Horner's scheme
-double cosTaylorH(const int nTerms, double x) {
-  double hornerScheme(vector<double> Poly, int degree, double x);
-  vector<double> b (2*nTerms); //numb. of polyn. coeficients should be the deg. + 1
-  for (int n = 0; n < nTerms; n++)
-    b[2 * n] = ((1 - 2 * (n % 2)));
-  return hornerScheme(b, 2 * nTerms - 2, x);
-}
-
-// Simple implmentation of Horner's Scheme
-double hornerScheme(vector<double> PolynomCoef, const int pDegree, double x) {
+double sinTaylor(int pDegree, double x)
+{
   double hSchemeRes = 0.0;
-  for (int i=pDegree; i >=0; i--){
-    hSchemeRes =  (PolynomCoef[i] + x * hSchemeRes) /  ((i == 0) ? 1 : i);
-    // cout << hSchemeRes << endl;
+  for (int i = 2 * pDegree - 2; i >= 0; i--){
+    hSchemeRes =  ((((1 + i) % 2 == 0) ? 2 - (i % 4)  : 0) +	// 0 -1 0 1 0...
+		   x * hSchemeRes) /				// Horner's recursion
+      ((i == 0) ? 1 : i);					// factorial factor
   }
   return hSchemeRes;
 }
 
-int main() {
+// taylor expansion for cos with Horner's scheme
+double cosTaylor(int pDegree, double x)
+{
+  double hSchemeRes = 0.0;
+  for (int i = 2 * pDegree - 2; i >= 0; i--){
+    hSchemeRes =  ((( i % 2 == 0) ? 1 - (i % 4)  : 0) + // 1 0 -1 0 1...
+		   x * hSchemeRes) /			// Horner's recursion
+      ((i == 0) ? 1 : i);				// factorial factor
+  }
+  return hSchemeRes;
+}
+
+int main()
+{
   vector<double> x_vec = {-1, 1, 2, 3, 5, 10};
   vector<unsigned int> N_vec = {1, 10, 20, 100};
 
@@ -41,10 +37,10 @@ int main() {
     cout << "Setting N = " << N << endl;
     for (auto x: x_vec) {
       cout << "  Setting x = " << x << endl;
-      cout << "    sin using Horner, recursive factorial, error:     "
-	   << abs(sin(x) - sinTaylorH(N, x)) << endl;
-      cout << "    cos using Horner, factorial with for-loop, error: "
-	   << abs(cos(x) - cosTaylorH(N, x)) << endl;
+      cout << "    sin using Horner, error: "
+	   << abs(sin(x) - sinTaylor(N, x)) << endl;
+      cout << "    cos using Horner, error: "
+      	   << abs(cos(x) - cosTaylor(N, x)) << endl;
     }
   }
   return 0;
