609c3c068b84b0b88ac734cc68c0135ce8babd29
andreasakarlsson <andreas.a.karlsson@gmail.com>
2016-09-29 19:13:13 +0200

    Minor bug fix plus added N+1-term

--- p1t1.cpp
+++ p1t1.cpp
@@ -7,7 +7,7 @@ using namespace std;
 double sinTaylor(int pDegree, double x)
 {
   double hSchemeRes = 0.0;
-  for (int i = 2 * pDegree - 2; i >= 0; i--){
+  for (int i = 2 * pDegree + 1; i >= 0; i--){                   // 2*n + 1 from the Taylor expansion
     hSchemeRes =  ((((1 + i) % 2 == 0) ? 2 - (i % 4)  : 0) +	// 0 -1 0 1 0...
 		   x * hSchemeRes) /				// Horner's recursion
       ((i == 0) ? 1 : i);					// factorial factor
@@ -19,7 +19,7 @@ double sinTaylor(int pDegree, double x)
 double cosTaylor(int pDegree, double x)
 {
   double hSchemeRes = 0.0;
-  for (int i = 2 * pDegree - 2; i >= 0; i--){
+  for (int i = 2 * pDegree; i >= 0; i--){               // 2*n from the Taylor expansion
     hSchemeRes =  ((( i % 2 == 0) ? 1 - (i % 4)  : 0) + // 1 0 -1 0 1...
 		   x * hSchemeRes) /			// Horner's recursion
       ((i == 0) ? 1 : i);				// factorial factor
@@ -37,10 +37,12 @@ int main()
     cout << "Setting N = " << N << endl;
     for (auto x: x_vec) {
       cout << "  Setting x = " << x << endl;
-      cout << "    sin using Horner, error: "
-	   << abs(sin(x) - sinTaylor(N, x)) << endl;
-      cout << "    cos using Horner, error: "
-      	   << abs(cos(x) - cosTaylor(N, x)) << endl;
+      cout << "    sin using Horner, error & N+1-term: "
+	   << abs(sin(x) - sinTaylor(N, x)) << "\t"
+	   << abs(sinTaylor(N + 1, x)) << endl;
+      cout << "    cos using Horner, error & N+1-term: "
+      	   << abs(cos(x) - cosTaylor(N, x)) << "\t"
+	   << abs(cosTaylor(N +1, x)) << endl;
     }
   }
   return 0;
