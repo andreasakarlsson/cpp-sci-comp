aa577b6c72b9822d058a8869f1f22748f279b17c
andreasakarlsson <andreas.a.karlsson@gmail.com>
2016-11-24 21:16:37 +0100

    Nicer pdf plot of grid points

--- project3/plotgrid.m
+++ project3/plotgrid.m
@@ -4,5 +4,10 @@ x = c(1:length(c)/2);  y = c(length(c)/2+1:end);
 
 f = figure;
 set(f, "visible", "off")
-plot(x,y,'o');
-print("gridpoints.png", "-dpng");
+plot(x,y,'.');
+my_axis = [-11 6 -1 4];
+axis(my_axis)
+axis equal
+set(gcf, 'PaperPosition', [0 0 16 5]); %Position plot at left hand corner with width 5 and height 5.
+set(gcf, 'PaperSize', [17 5]); %Set the paper to have width 5 and height 5.
+print("gridpoints.pdf", "-dpdf");
